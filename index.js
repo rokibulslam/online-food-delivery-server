4dcd5c78fa3b443a0a678a5a634b39024bf604b5
rokibulslam <rokibul1003@gmail.com>
2022-02-05 11:00:27 +0600

    delete, update status api created

--- index.js
+++ index.js
@@ -6,63 +6,104 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
-// Middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connecting MongoDB database 
+// Connecting MongoDB database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2efaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// API 
+// API
 async function run() {
-    
-  try { 
-      // Creating Collection at MongoDB
-        await client.connect();
-        console.log("server connected");
-        const database = client.db("hungry");
-        const foodCollection = database.collection("allFoods");
-        
-        // Find all Foods 
-        app.get("/foods", async (req, res) => {
-          const cursor = foodCollection.find({});
-          const allFoods = await cursor.toArray();
-          res.send(allFoods);
-        });
-        // Get purchase item from all foods
-        app.get("/foods/:id", async (req, res) => {
-          const id = req.params.id;
-          console.log(id)
-          const query = {
-            _id: ObjectId(id)
-          }
-          const product = await foodCollection.findOne(query)
-          console.log(product)
-          res.json(product)
-        });    
-    
-        // Add a Food 
-        app.post("/foods", async (req, res) => {
-          const food = req.body;
-          console.log("hit the post api", food);
-          const result = await foodCollection.insertOne(food);
-          console.log(result);
-          res.json(result);
-        });
+  try {
+    // Creating Collection at MongoDB
+    await client.connect();
+    console.log("server connected");
+    const database = client.db("hungry");
+    const foodCollection = database.collection("allFoods");
+    const orderCollection = database.collection("orders");
 
+    // Find all Foods
+    app.get("/foods", async (req, res) => {
+      const cursor = foodCollection.find({});
+      const allFoods = await cursor.toArray();
+      res.send(allFoods);
+    });
 
-    } finally {
-      // await client.close()
-    }
+    // Get Users Order
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+    // Search food with product id
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {
+        _id: ObjectId(id),
+      };
+      const product = await foodCollection.findOne(query);
+      console.log(product);
+      res.json(product);
+    });
+    // Search Orderd food by email
+    app.get("/orders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        email: req.params.email,
+      };
+      const result = await orderCollection.find(query).toArray();
+      console.log(result);
+      res.json(result);
+    });
+    // Update Ordered Product's status 
+    app.put("/status/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInfo = req.body;
+      const result = await orderCollection.updateOne(
+        { _id: ObjectId(id) },
+        { $set: { status: updateInfo.status } }
+      );
+      res.send(result);
+    });
+    // Delete Orderd Item by ID
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // Add a Food
+    app.post("/foods", async (req, res) => {
+      const food = req.body;
+      console.log("hit the post api", food);
+      const result = await foodCollection.insertOne(food);
+      console.log(result);
+      res.json(result);
+    });
+
+    // Add Order
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      console.log("hit the post api", order);
+      const result = await orderCollection.insertOne(order);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // await client.close()
+  }
 }
 
 run().catch(console.dir);
 
-// Checking server 
+// Checking server
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
